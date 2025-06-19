f25f177a38ed924333442f186dd17d0990d5b1c9
Menna Ayman <mennaaymangalal4@gmail.com>
2025-06-19 16:39:14 +0300

    making team page

---
+++ src/Pages/Team/Data.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+export const rows = [
+  { id: 1, name: 'Tommy', Email: 'theCommunityversion@gmail.com', Age: 14 ,Phone: "(020)1111557790" , Access : "Admin" },
+  { id: 2, name: 'Jack', Email: 'Communityversion@gmail.com', Age: 35 ,Phone: "(020)1018376651" , Access : "Admin" },
+  { id: 3, name: 'Roze', Email: 'theCommunityversion@gmail.com' , Age: 40 ,Phone: "0(021)566789841" , Access : "Admin"},
+  { id: 4, name: 'Ashraff', Email: 'theCommunityversion@gmail.com', Age: 88 ,Phone: "(020)1183765413" , Access : "Admin" },
+  { id: 5, name: 'Nissan',Email: 'teCommunityversion@gmail.com', Age: 90 ,Phone: "(020)1019181726" , Access : "Manger" },
+  { id: 6, name: 'Farouk', Email: 'teCommunityversion@gmail.com', Age: 24 ,Phone: "(020)1112257791" , Access : "Manger" },
+  { id: 7, name: 'Musilm',Email: 'heCommunityversion@gmail.com', Age: 55 ,Phone: "(020)1515267752" , Access : "User" },
+  { id: 8, name: 'Ermak', Email: 'theCommunityversion@gmail.com', Age: 70 ,Phone: "(020)1029384756" , Access : "User" },
+  { id: 9, name: 'Gordal', Email: 'teCommunityversion@gmail.com', Age: 64 ,Phone: "(020)1111929576" , Access : "User" },
+];
+
+
+
+export default function Data() {
+  return (
+    <>
+      
+    </>
+  )
+}
--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import { DataGrid } from '@mui/x-data-grid';
+import { Box, Typography, useTheme } from '@mui/material';
+import { rows } from './Data';
+import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
+import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
+import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined';
+
+
+  
 
 export default function Team() {
+const theme = useTheme()
+
+
+ const columns = [
+  { field: 'id', headerName: 'id',  headerAlign: "center"  , align: "center" , width : 33},
+  { field: 'name', headerName: 'name', flex: 1 , headerAlign: "center" , align: "center" },
+  { field: 'Email', headerName: 'Email', flex: 1 , headerAlign: "center"  , align: "center"  },
+  { field: 'Age', headerName: 'Age', flex: 1 , headerAlign: "center" , align: "center" },
+  { field: 'Phone', headerName: 'Phone', flex: 1 , headerAlign: "center"  , align: "center"  },
+  { field: 'Access', headerName: 'Access', flex: 1 , headerAlign: "center" , align: "center"
+    , renderCell:( {row : { Access }}) => {
+     return  <Box
+      sx={{
+      backgroundColor: Access === "Admin" ? theme.palette.primary.dark  : Access === "Manger" ? theme.palette.secondary.dark :" #3da58a"
+      , p: "5px"
+       , width: "99px"
+       , margin: "auto"       
+       , borderRadius:"3px"
+       , display: "flex"
+       ,justifyContent:"space-evenly",
+       mt: 1
+       }}>
+      {Access === "Admin" && <AdminPanelSettingsOutlinedIcon fontSize='small' sx={{color:"white"}}/>} 
+      {Access === "Manger" && <SecurityOutlinedIcon fontSize='small' sx={{color:"white"}}/>} 
+      {Access === "User" && <LockOpenOutlinedIcon fontSize='small' sx={{color:"white"}}/>} 
+      
+    <Typography sx={{fontSize: "13px" , color: "white"}}>{Access}</Typography>      
+    </Box>
+  }
+   },
+];
+
   return (
     <>
-     
+    <Typography sx={{color: theme.palette.info.main ,fontSize: 24 , fontWeight:"bold"}}>TEAM</Typography>
+    <Typography sx={{pb:2}}>Managing the Team Members</Typography>
+     <div style={{ height: 600, width: '98%', margin: "auto" }}>
+      <DataGrid rows={rows} columns={columns} />
+    </div>
     </>
   )
 }
