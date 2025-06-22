57e1150b0d31d63ca0631bad8a32c85de642232b
Menna Ayman <mennaaymangalal4@gmail.com>
2025-06-22 16:33:25 +0300

    making line chart title

--- src/Pages/Line/Line.jsx
+++ src/Pages/Line/Line.jsx
@@ -1,9 +1,23 @@
+import { Typography, useTheme } from '@mui/material';
 import React from 'react'
 
 export default function Line() {
+   const theme = useTheme();
   return (
     <>
-      
+     <Typography
+        sx={{
+          color: theme.palette.info.main,
+          fontSize: 24,
+          fontWeight: "bold",
+        }}
+      >
+        Line Chart
+      </Typography>
+      <Typography sx={{ pb: 2 }}>
+       Simple Line Chart
+      </Typography>
+   
     </>
   )
 }
